9079d624594eea85cbe7d2a9ba3cbe18ee6d81a7
sharanagouda.k <sharanagouda.k@robosoftin.com>
2020-01-13 13:33:54 +0530

    list design

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -2,12 +2,33 @@ import React, { Component} from 'react';
 import Button from '@material-ui/core/Button';
 import {connect} from "react-redux";
 import {fetchUserData } from "../../actionCreators/products.actionCreator";
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
 
 class Home extends Component{
+  
   constructor(props){
     super(props);
     this.state = {
     }
+    
   }
 
   componentDidMount() {
@@ -26,6 +47,18 @@ class Home extends Component{
             <Button variant="contained" color="primary">
             Hello Home
           </Button>
+              <div className="flex-two">
+              {this.props.app.map((item) => (
+                     <List component="nav" key={item.id} aria-label="main mailbox folders" style={{ display:"flex",flex:1, flexDirection:"row"}}>
+                          <ListItemAvatar>
+                              <Avatar>
+                                <BeachAccessIcon />
+                              </Avatar>
+                            </ListItemAvatar>
+                           <ListItemText primary={item.title} secondary={item.body} />
+                     </List>
+              ))}
+              </div>
             </div>
           </div>
         );
