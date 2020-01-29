c2a03548dc88408445befeb9d9694b0e770abc41
sharanagouda.k <sharanagouda.k@robosoftin.com>
2020-01-29 14:46:24 +0530

    searchScreen

--- src/container/SearchFlight/index.tsx
+++ src/container/SearchFlight/index.tsx
@@ -4,31 +4,22 @@ import { fade, Theme} from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import {connect} from "react-redux";
 import {compose} from "redux";
+import { makeStyles } from "@material-ui/styles";
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
     overflow: 'hidden',
-    padding: theme.spacing(0, 3),
   },
   search: {
     display:"flex",
     position: 'relative',
     border: "2px solid red",
     borderRadius: 4,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
+    width: '15%',
   },
   searchIcon: {
     display:'flex',
-    width: theme.spacing(5),
     top:5,
     height: '100%',
     position: 'relative',
@@ -40,34 +31,17 @@ const styles = (theme) => ({
     color: 'primary',
   },
   inputInput: {
-    padding: theme.spacing(1, 7, 1, 7),
-    transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
     backgroundColor:"lightblue"
 }
-});
+}));
 
 
 
 
-class SearchFlight extends Component{
+function SearchFlight(){
+  const classes = useStyles();
 
-  
-  constructor(props){
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  componentDidMount() {
- }
-
-  render(){
-    const {classes} =this.props;
       return (
           <div className={classes.root}>
               <br/>
@@ -86,11 +60,6 @@ class SearchFlight extends Component{
           </div>
           </div>
         );
-  }
 }
 
-const mapStateToProps = (state)=> ({
-})
-const mapDispatchToProps =(dispatch)=>({
-}) 
-export default  compose(withStyles(styles),connect(mapStateToProps,mapDispatchToProps)) (SearchFlight);
+export default SearchFlight;
